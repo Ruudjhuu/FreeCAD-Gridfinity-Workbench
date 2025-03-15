d46d26034db83f7051e8d704fee9bcf24bc6c5f4
Grzegorz Kwacz <gkwacz@hotmail.com>
2025-03-15 18:11:49 +0100

    add volume tests

--- freecad/gridfinity_workbench/test_gridfinity.py
+++ freecad/gridfinity_workbench/test_gridfinity.py
@@ -5,6 +5,8 @@ from tempfile import gettempdir
 import FreeCAD as fc  # noqa: N813
 import FreeCADGui as fcg  # noqa: N813
 
+import freecad
+
 TEMPDIR = Path(gettempdir())
 DOC_NAME = "GridfinityDocument"
 
@@ -70,3 +72,80 @@ class TestSave(unittest.TestCase):
 
         doc.save()
         fc.closeDocument(doc.Name)
+
+
+class TestVolumes(unittest.TestCase):
+    def setUp(self) -> None:
+        self.doc = fc.newDocument(DOC_NAME)
+
+    def test_custom_bin_rectangle(self) -> None:
+        freecad.gridfinity_workbench.custom_shape.get_layout = lambda: [[True, True], [True, True]]
+        fcg.Command.get("CreateBinBlank").run()
+        obj1 = fcg.ActiveDocument.ActiveObject.Object
+        fcg.Command.get("CreateCustomBin").run()
+        obj2 = fcg.ActiveDocument.ActiveObject.Object
+        self.assertAlmostEqual(obj1.Shape.Volume, obj2.Shape.Volume)
+
+    def test_custom_bin_l(self) -> None:
+        freecad.gridfinity_workbench.custom_shape.get_layout = lambda: [[True, True, True], [True]]
+        fcg.Command.get("CreateLBinBlank").run()
+        obj1 = fcg.ActiveDocument.ActiveObject.Object
+        fcg.Command.get("CreateCustomBin").run()
+        obj2 = fcg.ActiveDocument.ActiveObject.Object
+        self.assertAlmostEqual(obj1.Shape.Volume, obj2.Shape.Volume)
+
+    def test_bin_blank(self) -> None:
+        fcg.Command.get("CreateBinBlank").run()
+        obj = fcg.ActiveDocument.ActiveObject.Object
+        obj.MagnetHoles = False
+        obj.recompute()
+        self.assertAlmostEqual(obj.Shape.Volume, 288887.4126750665)
+        obj.RecessedTopDepth = 3
+        obj.recompute()
+        self.assertAlmostEqual(obj.Shape.Volume, 270272.25637141115)
+        obj.RecessedTopDepth = 0
+        obj.StackingLip = False
+        obj.recompute()
+        self.assertAlmostEqual(obj.Shape.Volume, 286724.9489979051)
+
+    def test_bin_base(self) -> None:
+        fcg.Command.get("CreateBinBase").run()
+        obj = fcg.ActiveDocument.ActiveObject.Object
+        obj.MagnetHoles = False
+        obj.recompute()
+        self.assertAlmostEqual(obj.Shape.Volume, 43118.696363716575)
+
+    def test_bin(self) -> None:
+        fcg.Command.get("CreateSimpleStorageBin").run()
+        obj = fcg.ActiveDocument.ActiveObject.Object
+        obj.MagnetHoles = False
+        obj.recompute()
+        self.assertAlmostEqual(obj.Shape.Volume, 58187.690383500565)
+        obj.StackingLip = False
+        obj.recompute()
+        self.assertAlmostEqual(obj.Shape.Volume, 54707.472846542325)
+
+    def test_eco_bin(self) -> None:
+        fcg.Command.get("CreateEcoBin").run()
+        obj = fcg.ActiveDocument.ActiveObject.Object
+        obj.MagnetHoles = False
+        obj.recompute()
+        self.assertAlmostEqual(obj.Shape.Volume, 24728.976287436377)
+
+    def test_baseplate(self) -> None:
+        fcg.Command.get("CreateBaseplate").run()
+        obj = fcg.ActiveDocument.ActiveObject.Object
+        self.assertAlmostEqual(obj.Shape.Volume, 5034.2316047825325)
+
+    def test_magnet_baseplate(self) -> None:
+        fcg.Command.get("CreateMagnetBaseplate").run()
+        obj = fcg.ActiveDocument.ActiveObject.Object
+        self.assertAlmostEqual(obj.Shape.Volume, 12606.095388468213)
+
+    def test_screw_together_baseplate(self) -> None:
+        fcg.Command.get("CreateScrewTogetherBaseplate").run()
+        obj = fcg.ActiveDocument.ActiveObject.Object
+        self.assertAlmostEqual(obj.Shape.Volume, 22204.66064178137)
+
+    def tearDown(self) -> None:
+        fc.closeDocument(DOC_NAME)
